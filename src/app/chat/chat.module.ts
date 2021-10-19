9ce267791a4f4135cab64894dbbb48dcaa2ce6d7
Marina Parkhomenko <qwertf030915@gmail.com>
2021-10-19 14:12:21 +0300

    style chat

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { ChatComponent } from './chat.component';
   ],
   imports: [
     CommonModule,
-    ChatRoutingModule
+    ChatRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule
   ]
 })
 export class ChatModule { }
